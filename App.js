9c20adca2c7ac3a34b59dd0d99782b7ca9ddcca8
jhs512 <jangka512@gmail.com>
2021-09-08 10:52:21 +0900

    Constants.statusBarHeight 를 사용해서, 상태바와 겹치지 않도록

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
+import Constants from "expo-constants";
 import tw from "tailwind-react-native-classnames";
 import { Button, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={tw`bg-red-500 flex-grow justify-center items-center`}>
+    <View
+      style={tw.style(`bg-red-500 flex-grow items-center`, {
+        paddingTop: Constants.statusBarHeight,
+      })}
+    >
       <Button
         title="버튼"
         onPress={() => {
