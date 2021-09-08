f6cc91772e121a7b69f5936f5bcd9896edf40181
jhs512 <jangka512@gmail.com>
2021-09-08 10:48:46 +0900

    각 기기별 로그내용 확인하는 방법

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import tw from "tailwind-react-native-classnames";
-import { Text, View } from "react-native";
+import { Button, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={tw`bg-red-500 flex-grow justify-center items-center`}>
-      <Text style={tw`bg-green-500 p-10 rounded`}>잘된다!</Text>
+      <Button
+        title="버튼"
+        onPress={() => {
+          console.log("HI");
+        }}
+      ></Button>
+      <Text style={tw`bg-green-500 p-10 rounded`}>잘된다!111</Text>
       <StatusBar style="auto" />
     </View>
   );
